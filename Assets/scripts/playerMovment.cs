a6099fc117c68c8e71b8fa2e7c5b2f67603ad9d5
enigmaDevelopments <enigmarezac@gmail.com>
2026-07-23 14:00:16 -0400

    make rewind

--- Assets/scripts/playerMovment.cs
+++ Assets/scripts/playerMovment.cs
@@ -1,32 +1,78 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class playerMovment : MonoBehaviour
 {
+    private struct Location
+    {
+        public Vector2 postiotion;
+        public float time;
+    }
+
+    public PlayerInput input;
     [Range(0, 10)]
     public float speed;
-    public float startingTime; 
-    private Vector2 movement;
+    public float startingTime;
+    private List<Location> Locations = new List<Location>();
+    private bool rewindTap;
+    private bool rewindHold;
+
+
 
     private void Start()
     {
         Timer.time = startingTime;
+        input.actions["Rewind"].performed += OnRewindPress;
+        input.actions["Rewind"].canceled += OnRewindRelease;
     }
 
     private void Update()
     {
+        if (rewindTap)
+            Timer.time += .005f;
+        else if (rewindHold)
+            Timer.time += Time.deltaTime;
+        if (rewindTap || rewindHold)
+        {
+            Timer.time = Mathf.Min(Timer.time, startingTime);
+            for (int i = Locations.Count - 1; 0 <= i; i--)
+            {
+                if (Timer.time <= Locations[i].time)
+                {
+                    transform.position = Locations[i].postiotion;
+                    Timer.time = Locations[i].time;
+                    while (i < Locations.Count)
+                        Locations.RemoveAt(Locations.Count - 1);
+                    break;
+                }
+            }
+            rewindTap = false;
+            return;
+        }
+
         if (Timer.time <= 0)
         {
             Timer.time = 0;
             return;
         }
+        Vector2 movement = input.actions["Move"].ReadValue<Vector2>();
         transform.position += (Vector3)movement * speed * Time.deltaTime;
         Timer.time -= movement.magnitude * Time.deltaTime;
+        Locations.Add(
+            new Location { 
+            postiotion = transform.position,
+            time = Timer.time });
     }
-
-    public void OnMove(InputValue value)
+    public void OnRewindPress(InputAction.CallbackContext context)
     {
-        movement = value.Get<Vector2>();
+        rewindTap = context.interaction is TapInteraction;
+        rewindHold = context.interaction is HoldInteraction;
+    }
+    public void OnRewindRelease(InputAction.CallbackContext context)
+    {
+        rewindHold = false;
     }
 
 
