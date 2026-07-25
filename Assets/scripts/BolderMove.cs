ed4a09a57b1415a286c0ca44f03e4e4c3bea4351
enigmaDevelopments <enigmarezac@gmail.com>
2026-07-25 19:02:33 -0400

    fixed bolder

--- Assets/scripts/BolderMove.cs
+++ Assets/scripts/BolderMove.cs
@@ -11,16 +11,26 @@ public class BolderMove : MonoBehaviour
     void FixedUpdate()
     {
         float distence = velocity * Timer.fixedDeltaTime;
-        rb.MovePosition(rb.position + Vector2.right * distence);
-        bolder.Rotate(0, 0, distence * -360 / Mathf.PI);
+        Move(distence);
         rb.linearVelocity = Vector2.zero;
         distanceMoved += distence;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+            return;
+        velocity *= -1;
+        distanceMoved = 0;
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 6 || distanceMoved < .125)
             return;
-        velocity *= -1;
-        distanceMoved = 0;
+        Move(Mathf.Sign(velocity) * .25f);
+    }
+    private void Move(float distence)
+    {
+        rb.MovePosition(rb.position + Vector2.right * distence);
+        bolder.Rotate(0, 0, distence * -360 / Mathf.PI);
     }
 }
