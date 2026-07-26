031a0663c911fd439e244a5c1e4643d514c5fbf3
enigmaDevelopments <enigmarezac@gmail.com>
2026-07-25 20:39:27 -0400

    fix that stupid bolder for the hundredth time

--- Assets/scripts/BolderMove.cs
+++ Assets/scripts/BolderMove.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BolderMove : MonoBehaviour
@@ -5,7 +6,6 @@ public class BolderMove : MonoBehaviour
     public Transform bolder;
     public Rigidbody2D rb;
     public float velocity;
-    private float distanceMoved;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -13,23 +13,22 @@ public class BolderMove : MonoBehaviour
         float distence = velocity * Timer.fixedDeltaTime;
         Move(distence);
         rb.linearVelocity = Vector2.zero;
-        distanceMoved += distence;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 6)
             return;
         velocity *= -1;
-        distanceMoved = 0;
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 6 || distanceMoved < .125)
-            return;
-        Move(Mathf.Sign(velocity) * .25f);
+        ContactPoint2D contact = collision.GetContact(0);
+        Move((.125f - contact.separation) * Mathf.Sign(contact.normal.x));
     }
     private void Move(float distence)
     {
+        if (distence == 0)
+            return;
         rb.MovePosition(rb.position + Vector2.right * distence);
         bolder.Rotate(0, 0, distence * -360 / Mathf.PI);
     }
